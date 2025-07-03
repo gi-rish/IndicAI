b02b1004c2ec9256fbaa6cb48dea5f562bc20138
girishnarayan <girishn@newstreettech.com>
2025-07-03 10:06:22 +0530

    chroma db

--- chromaDb.py
+++ chromaDb.py
@@ -4,7 +4,7 @@ import chromadb
 from sentence_transformers import SentenceTransformer
 
 # Connect to Chroma server
-client = chromadb.HttpClient(host="localhost", port=8000)
+client = chromadb.HttpClient(host="3.6.132.24", port=8000)
 collection = client.get_or_create_collection("language_embeddings")
 
 # Define sample phrases per language (in Latin script for now)
